c24b755aa5f8b2d21ce5791ad144211b2ae335e3
Zogymac <sokal.2001@mail.ru>
2024-06-16 23:28:45 +0300

    Add Car table, configure one-to-one relationship and configure query to fetch User by car details

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,11 +1,14 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -15,15 +18,32 @@ public class UserDaoImp implements UserDao {
    private SessionFactory sessionFactory;
 
    @Override
-   public void add(User user) {
+   public void add(User user, Car car) {
       sessionFactory.getCurrentSession().save(user);
+      sessionFactory.getCurrentSession().save(car);
    }
 
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query=sessionFactory.getCurrentSession()
+              .createQuery("select u from User u join u.car");
       return query.getResultList();
    }
+
+   @Override
+   @Transactional
+   public User getUserByCar(String model, int series) {
+      String hql = ("select u from User u join u.car where u.car.model = :model" +
+                           " and u.car.series = :series");
+      Query query = sessionFactory.getCurrentSession().createQuery(hql);
+
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+
+      return (User) query.getResultList().get(0);
+   }
+
+
 
 }
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,46 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "car")
+public class Car implements Serializable {
+
+    @Id
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Car() {
+    }
+
+
+    public Car(User user, String model, int series) {
+        this.user = user;
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
